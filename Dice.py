93a765aef96cf9e107f21eb177d70a3db1bc6623
Ryan Sheehan <rsheehan@umass.edu>
2019-04-05 19:22:57 -0400

    poop edit

--- Dice.py
+++ Dice.py
@@ -4,3 +4,4 @@ def Dice(weight1, weight2):
         [1,2,3,4,5,6] for normal and a weighted dice will be [1,2,3,4,5,6,6,6,6,6,6]
         Then select randomly from the list for the dice results. 
         Returns the sum of the two dices """
+poop
