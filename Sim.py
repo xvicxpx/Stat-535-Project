23ba1fb96f4b154f677b15b9743c3edd7eba0917
Sean Emerson <seanemerson@umass.edu>
2019-04-06 15:50:01 -0400

    Counts wins for many games

---
+++ Sim.py
@@ -0,0 +1,11 @@
+def sim(n = 100, weight1 = none, weight2 = none):
+    """
+    Takes in a number of games n, and the weights for two dice. 
+    Returns the total number of wins from n craps games
+    """
+    for i in range(n):
+        count = 0
+        if(play(weight1, weight2))
+            count++
+    return count
+    
