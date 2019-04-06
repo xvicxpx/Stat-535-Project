b085c7270067c245c7287aa531e0a02842e4bd52
Sean Emerson <seanemerson@umass.edu>
2019-04-06 16:01:31 -0400

    Generate win count in Dictionary of Lists
    Returns a dictionary where each key is a weighting on a set of dice, from (none, none) to (1, none) and (6, 6). Within each key is a list with positions 0 to 99, each storing the number of that many wins occured in the trials

--- Trials.py
+++ Trials.py
@@ -1,5 +1,36 @@
-def trials(N, n, weight1, weight2):
-    """ N = how many times will run
-        n = how many time run Sim()
-        will collect all the data in a CSV
-        """
+def trials(N = 1000, n = 100):
+    """ 
+    N = how many times will run
+    n = how many time run Sim()
+    will collect all the data into a dictionary of lists
+    """
+    data = {}
+    trial = [0 for i in range(100)]
+    test_wins = 0
+    
+    for test in range(N):
+        test_wins = sim(n = n)
+        trial[test_wins]++
+    
+    data[(none,none)] = trial
+    
+    for die1 in range (1,7):
+        
+        for die2 in range (die1, 7):
+            trial = [0 for i in range(100)]
+            
+            for test in range(N):
+                test_wins = sim(n = n, weight1 = die1, weight2 = die2)
+                trial[test_wins]++
+                
+            data[(die1, die2)] = trial
+        
+        trial = [0 for i in range(100)]    
+        
+        for test in range(N):
+            test_wins = sim(n = n, weight1 = die1)
+            trial[test_wins]++
+            
+        data[(die1, none)] = trial
+        
+    return data
