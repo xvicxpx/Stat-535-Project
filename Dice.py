d4f7f18fce00ff7559d284c5040e5a92226a106f
achoubey <26352723+achoubey@users.noreply.github.com>
2019-04-10 12:56:12 -0400

    Update Dice.py

--- Dice.py
+++ Dice.py
@@ -1,6 +1,26 @@
+import random
 
 def Dice(weight1, weight2):
     """ weight1 and weight 2 will be a list 
         [1,2,3,4,5,6] for normal and a weighted dice will be [1,2,3,4,5,6,6,6,6,6,6]
         Then select randomly from the list for the dice results. 
         Returns the sum of the two dices """
+    # norm_dice = [1,2,3,4,5,6]
+    # rand = 
+    if (weight1 == null && weight2 == null)
+        return(random.randint(1,6))
+    elif(weight1 =! null)
+        return(weight1_func(weight1))
+    elif(weight2 =! null)
+        return(weight2_func(weight2))
+    else
+        return(both_weighted(weight1,weight2))
+    
+    
+def weight1():
+    
+    
+    
+    return()
+
+def weight2_func
