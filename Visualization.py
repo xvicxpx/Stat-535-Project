92d737c9623b522edaa38ad7dd027ae16c5e7bcc
Ryan Sheehan <rsheehan@umass.edu>
2019-04-12 12:19:39 -0400

    Rates Graph

--- Visualization.py
+++ Visualization.py
@@ -1,1 +1,25 @@
 #Visualization 
+
+"""
+Takes a dictionary of lists
+Creates a new Dictionary with the same keys but values are the average of the lists of the original
+Then plots them in a bar plot
+
+After examination we will make graphs of the best and worst performances
+"""
+
+import matplotlib.pyplot as plt
+import statistics
+
+rates_dict = {}
+
+def rateDic(dictionary):
+    for x in dictionary:
+        rates_dict[x] = statistics.mean(dictionary[x])
+    return rates_dict
+
+
+newDict = rateDic(testDict)
+
+plt.bar(range(len(newDict)), list(newDict.values()), align='center')
+plt.xticks(range(len(newDict)), list(newDict.keys()))
