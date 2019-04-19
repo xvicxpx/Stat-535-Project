5fb40a287b48ab2972feee3afb0490485cabbfdf
Ryan Sheehan <rsheehan@umass.edu>
2019-04-19 18:40:59 -0400

    submission

--- Visualization.py
+++ Visualization.py
@@ -10,15 +10,18 @@ After examination we will make graphs of the best and worst performances
 
 import matplotlib.pyplot as plt
 import statistics
+import Combinations as c
 
 rates_dict = {}
+testDict = c.combinations(N=100, n = 10)[0]
 
 def rateDic(dictionary):
     for x in dictionary:
-        rates_dict[x] = statistics.mean(dictionary[x])
+        rates_dict[x] = statistics.mean(dictionary[x]) 
     return rates_dict
 
-newDict = rateDic(_____) #Enter the dictionary name
+
+newDict = rateDic(testDict)
 
 plt.bar(range(len(newDict)), list(newDict.values()), align='center')
 plt.xticks(range(len(newDict)), list(newDict.keys()))
