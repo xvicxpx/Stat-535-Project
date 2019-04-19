9ea2720b182dda8af7e4c9320482a323dbd39716
Ryan Sheehan <rsheehan@umass.edu>
2019-04-19 18:57:17 -0400

    fixed axes

--- Visualization.py
+++ Visualization.py
@@ -7,21 +7,12 @@ Then plots them in a bar plot
 
 After examination we will make graphs of the best and worst performances
 """
-
 import matplotlib.pyplot as plt
-import statistics
 import Combinations as c
 
 rates_dict = {}
-testDict = c.combinations(N=100, n = 10)[0]
+testDict = c.combinations(N=100, n=10)
 
-def rateDic(dictionary):
-    for x in dictionary:
-        rates_dict[x] = statistics.mean(dictionary[x]) 
-    return rates_dict
-
-
-newDict = rateDic(testDict)
-
-plt.bar(range(len(newDict)), list(newDict.values()), align='center')
-plt.xticks(range(len(newDict)), list(newDict.keys()))
+plt.bar(range(len(testDict)), list(testDict.values()), align='center')
+plt.xticks(range(len(testDict)), list(testDict.keys()), rotation = 'vertical')
+plt.ylim([0.4 , 0.6])
